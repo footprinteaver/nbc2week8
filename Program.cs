1f3d47dcbdbefe3e4b60bca8fa9a07574bb5d6f0
baedonggeun <hispanie3@gmail.com>
2024-01-10 14:59:46 +0900

    battlemenu 코딩

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace _5week_assignment
+﻿
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _5week_assignment
 {
     //hi
     public class Character
@@ -9,6 +12,8 @@
         public int Atk { get; }
         public int Def { get; }
         public int Hp { get; }
+
+        public int HpFull = 100;
         public int Gold { get; }
 
         public Character(string name, string job, int level, int atk, int def, int hp, int gold)
@@ -23,126 +28,158 @@
         }
     }
 
-    //public class Item
-    //{
-    //    public string Name { get; }
-    //    public string Description { get; }
-    //    public int Type { get; }
-    //    public int Atk { get; }
-    //    public int Def { get; }
-    //    public int Hp { get; }
-    //    public bool isEquipped { get; set; }
+    public class Enemy
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public int Level { get; }
+        public int Atk { get; }
+        public int Hp { get; }
+        public int[] enemyCnt { get; }
 
-    //    public Item(string name, string description, int type, int atk, int def, int hp, bool isEquipped = false)
-    //    {
-    //        Name = name;
-    //        Description = description;
-    //        Type = type;
-    //        Atk = atk;
-    //        Def = def;
-    //        Hp = hp;
-    //        isEquipped = isEquipped;
-    //    }
+        public Enemy(string name, int level, int atk, int hp)
+        {
+            Name = name;
+            Level = level;
+            Atk = atk;
+            Hp = hp;
+        }
 
-    //    public static int ItemCnt = 0;
 
-    //    public void PrintItemStatDescription(bool withNumber = false, int idx = 0)
-    //    {
-    //        Console.Write("- ");
+        public static int enemyType = 0;
 
-    //        if (withNumber)
-    //        {
-    //            Console.ForegroundColor = ConsoleColor.DarkMagenta;
-    //            Console.Write("{0} ", idx);
-    //            Console.ResetColor();
-    //        }
 
-    //        if (isEquipped)
-    //        {
-    //            Console.Write("[");
-    //            Console.ForegroundColor = ConsoleColor.Cyan;
-    //            Console.Write("E");
-    //            Console.ResetColor();
-    //            Console.Write("]");
-    //        }
-    //        else
-    //        {
-    //            Console.Write(PadRightForMixedText(Name, 12));
-    //        }
 
-    //        Console.Write(" | ");
+    }
 
-    //        if (Atk != 0)
-    //        {
-    //            Console.Write($"Atk {(Atk >= 0 ? "+" : "")}{Atk}");
-    //        }
-    //        if (Def != 0)
-    //        {
-    //            Console.Write($"Def {(Def >= 0 ? "+" : "")}{Def}");
-    //        }
-    //        if (Hp != 0)
-    //        {
-    //            Console.Write($"Hp {(Hp >= 0 ? "+" : "")}{Hp}");
-    //        }
+        #region 상점 주석 처리
+        //public class Item
+        //{
+        //    public string Name { get; }
+        //    public string Description { get; }
+        //    public int Type { get; }
+        //    public int Atk { get; }
+        //    public int Def { get; }
+        //    public int Hp { get; }
+        //    public bool isEquipped { get; set; }
 
-    //        Console.Write(" | ");
+        //    public Item(string name, string description, int type, int atk, int def, int hp, bool isEquipped = false)
+        //    {
+        //        Name = name;
+        //        Description = description;
+        //        Type = type;
+        //        Atk = atk;
+        //        Def = def;
+        //        Hp = hp;
+        //        isEquipped = isEquipped;
+        //    }
 
-    //        Console.WriteLine(Description);
+        //    public static int ItemCnt = 0;
 
-    //    }
+        //    public void PrintItemStatDescription(bool withNumber = false, int idx = 0)
+        //    {
+        //        Console.Write("- ");
 
-    //    public static int GetPrintableLength(string str)
-    //    {
-    //        int length = 0;
-    //        foreach (char c in str)
-    //        {
-    //            if (char.GetUnicodeCategory(c) == System.Globalization.UnicodeCategory.OtherLetter)
-    //            {
-    //                length += 2;
-    //            }
-    //            else
-    //            {
-    //                length += 1;
-    //            }
-    //        }
+        //        if (withNumber)
+        //        {
+        //            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        //            Console.Write("{0} ", idx);
+        //            Console.ResetColor();
+        //        }
 
-    //        return length;
-    //    }
+        //        if (isEquipped)
+        //        {
+        //            Console.Write("[");
+        //            Console.ForegroundColor = ConsoleColor.Cyan;
+        //            Console.Write("E");
+        //            Console.ResetColor();
+        //            Console.Write("]");
+        //        }
+        //        else
+        //        {
+        //            Console.Write(PadRightForMixedText(Name, 12));
+        //        }
 
-    //    public static string PadRightForMixedText(string str, int totalLength)
-    //    {
-    //        int currentLength = GetPrintableLength(str);
-    //        int padding = totalLength - currentLength;
-    //        return str.PadRight(str.Length + padding);
-    //    }
-    //}
+        //        Console.Write(" | ");
 
+        //        if (Atk != 0)
+        //        {
+        //            Console.Write($"Atk {(Atk >= 0 ? "+" : "")}{Atk}");
+        //        }
+        //        if (Def != 0)
+        //        {
+        //            Console.Write($"Def {(Def >= 0 ? "+" : "")}{Def}");
+        //        }
+        //        if (Hp != 0)
+        //        {
+        //            Console.Write($"Hp {(Hp >= 0 ? "+" : "")}{Hp}");
+        //        }
+
+        //        Console.Write(" | ");
+
+        //        Console.WriteLine(Description);
+
+        //    }
+
+        //    public static int GetPrintableLength(string str)
+        //    {
+        //        int length = 0;
+        //        foreach (char c in str)
+        //        {
+        //            if (char.GetUnicodeCategory(c) == System.Globalization.UnicodeCategory.OtherLetter)
+        //            {
+        //                length += 2;
+        //            }
+        //            else
+        //            {
+        //                length += 1;
+        //            }
+        //        }
+
+        //        return length;
+        //    }
+
+        //    public static string PadRightForMixedText(string str, int totalLength)
+        //    {
+        //        int currentLength = GetPrintableLength(str);
+        //        int padding = totalLength - currentLength;
+        //        return str.PadRight(str.Length + padding);
+        //    }
+        //}
+        #endregion
 
 
     internal class Program
     {
-        private static Character _player;
-        //private static Item[] _items;
+            private static Character _player;
+            //private static Item[] _items;
+            private static Enemy[] _enemys;
 
-        static void Main(string[] args)
-        {
-            GameDataSetting();
-            PrintStartLogo();
-            startMenu();
+            static void Main(string[] args)
+            {
+                GameDataSetting();
+                PrintStartLogo();
+                startMenu();
 
-        }
+            }
 
 
         private static void GameDataSetting()
         {
-            _player = new Character("Chad", "전사", 1, 10, 5, 10, 1500);
-           // _items = new Item[10];
+            _player = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
+            _enemys = new Enemy[3];
+            // _items = new Item[10];
 
-           // AddItem(new Item("무쇠 갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 0, 0, 5, 0));
-           // AddItem(new Item("낡은 검", "쉽게 볼 수 있는 낡은 검입니다.", 1, 2, 0, 0));
+            // AddItem(new Item("무쇠 갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 0, 0, 5, 0));
+            // AddItem(new Item("낡은 검", "쉽게 볼 수 있는 낡은 검입니다.", 1, 2, 0, 0));
+
+            AddEnemy(new Enemy("미니언", 2, 5, 15));
+            AddEnemy(new Enemy("공허충", 3, 9, 10));
+            AddEnemy(new Enemy("대포미니언", 5, 8, 25));
 
         }
 
+        #region 상점 주석 처리
         //static void AddItem(Item item)
         //{
         //    if (Item.ItemCnt == 10)
@@ -153,7 +190,21 @@
         //    _items[Item.ItemCnt] = item;
         //    Item.ItemCnt++;
         //}
+        #endregion
 
+        //enemy 추가 함수
+        static void AddEnemy(Enemy enemy)
+        {
+            if(Enemy.enemyType == 4)
+            {
+                return;
+            }
+
+            _enemys[Enemy.enemyType] = enemy;
+            Enemy.enemyType++;
+        }
+
+        //시작 로고 화면
         private static void PrintStartLogo()
         {
             Console.WriteLine("====================================================================");
@@ -178,14 +229,15 @@
 
         }
 
+        //게임 시작 화면
         static void startMenu()
         {
             Console.Clear();
-            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
-            Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("이제 전투를 시작할 수 있습니다.");
             Console.WriteLine();
             Console.WriteLine("1. 상태 보기");
-            //Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("2. 전투 시작");
             Console.WriteLine();
 
             switch (CheckValidInput(1, 2))
@@ -193,14 +245,16 @@
                 case 1:
                     StatusMenu();
                     break;
-                //case 2:
-                //    InventoryMenu();
-                //    break;
+                case 2:
+                    battleMenu();
+                    break;
             }
 
 
         }
 
+        
+        //입력 처리 함수
         private static int CheckValidInput(int min, int max)
         {
             int keyInput;
@@ -216,6 +270,7 @@
 
         }
 
+        //제대로 입력했는지 확인하는 함수
         private static bool CheckIfValid(int keyInput, int min, int max)
         {
             if (min <= keyInput && keyInput <= max)
@@ -224,9 +279,13 @@
             }
             else
             {
+                Console.Clear();
+                Console.WriteLine("잘못된 입력입니다.");
                 return false;
             }
         }
+
+        //text 색상 변경 함수
         private static void ShowHighlightedText(string text)
         {
             Console.ForegroundColor = ConsoleColor.Magenta;
@@ -234,6 +293,7 @@
             Console.ResetColor();
         }
 
+        //문자열 나눠서 색상 변경 함수
         private static void PrintTextWithHighlights(string s1, string s2, string s3 = "")
         {
             Console.Write(s1);
@@ -243,6 +303,7 @@
             Console.WriteLine(s3);
         }
 
+        //상태창
         private static void StatusMenu()
         {
             Console.Clear();
@@ -257,7 +318,7 @@
             PrintTextWithHighlights("공격력 : ", _player.Atk.ToString());
             PrintTextWithHighlights("방어력 : ", _player.Def.ToString());
             PrintTextWithHighlights("체력 : ", _player.Hp.ToString());
-            PrintTextWithHighlights("골드 : ", _player.Gold.ToString());
+            PrintTextWithHighlights("골드 : ", _player.Gold.ToString(), " G");
             Console.WriteLine();
             Console.WriteLine("0. 뒤로 가기");
             Console.WriteLine();
@@ -270,6 +331,7 @@
             }
         }
 
+        #region 상점 주석 처리
         //private static int getSumBonusAtk()
         //{
         //    int sum = 0;
@@ -373,6 +435,88 @@
         //{
         //    _items[idx].isEquipped = !_items[idx].isEquipped;
         //}
+        #endregion
+
+        //전투
+        private static void battleMenu()
+        {
+            int[] enemyCnt = RandomNumberArrGenerator(1, 5);
+
+
+            Console.Clear();
+            ShowHighlightedText("Battle!!");
+            Console.WriteLine("\n");
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("적 정보");
+            Console.ResetColor();
+            Console.Write("]");
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < enemyCnt.Length; i++)
+            {
+                Random rand = new Random();
+                int enemyLv = rand.Next(0, 3);
+                enemyCnt[i] = enemyLv;
+
+                Console.Write("Lv.{0} {1}   ", _enemys[enemyLv].Level, _enemys[enemyLv].Name);
+                Console.Write("Hp : {0}    ", _enemys[enemyLv].Hp);
+                Console.WriteLine("Atk : {0}", _enemys[enemyLv].Atk);
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("\n");
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("내 정보");
+            Console.ResetColor();
+            Console.Write("]");
+            Console.WriteLine("\n");
+            Console.WriteLine("Lv.{0}   {1}  ({2})",_player.Level, _player.Name, _player.Job);
+            Console.WriteLine("HP {0} / {1}", _player.Hp, _player.HpFull);
+            Console.WriteLine("Atk : {0}", _player.Atk);
+            Console.WriteLine("Def : {0}", _player.Def);
+            Console.WriteLine();
+            Console.WriteLine("1. 공격");
+            Console.WriteLine("2. 도망");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+            switch (CheckValidInput(1, 2))
+            {
+                case 0:
+                    Attack();
+                    break;
+                case 1:
+                    runaway();
+                    break;
+            }
+
+        }
+
+
+        public static int[] RandomNumberArrGenerator(int min, int max)
+        {
+            Random rand = new Random();
+            int number = rand.Next(min, max);
+            int[] enemyNumber = new int[number];
+
+            return enemyNumber;
+        }
+
+        private static void Attack()
+        {
+            throw new NotImplementedException();
+
+            // int enemyToPlayer = _enemys[].Atk - _player.Def;
+        }
+
+        private static void runaway()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 
 
